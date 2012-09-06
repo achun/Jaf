c6a6955b9775f95af0f3aa9ed1dc065bffeb12f7
achun <achun.shx@qq.com>
2012-09-06 13:49:26 +0800

    ɾ��δʵ�ֵ����ļ�

--- Framework/Route/Map.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace Jaf;
-final class Route_Map implements Route_Interface {
-	protected $_ctl_router ;
-	protected $_delimeter ;
-	public function __construct ( $controller_prefer, $delimiter){}
-	public function route ( $request){}
-}
